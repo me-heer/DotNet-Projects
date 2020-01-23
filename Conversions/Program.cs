24f0668caf0127432ccf789cd65430f1b24e1074
Mihir Joshi <mihir67mj@gmail.com>
2020-01-23 09:23:16 +0530

    Conversions

--- Conversions/Program.cs
+++ Conversions/Program.cs
@@ -4,18 +4,87 @@ namespace Conversions
 {
     class Program
     {
-        public static int BinaryToDecimal(string Binary) {
-            int Decimal = 0;
-            for(int i = Binary.Length - 1; i >= 0; i--)
+        public static string BinaryToDecimal(string Binary) {
+            int Decimal = 0, temp , i = 0;
+            int binaryValue = int.Parse(Binary);
+            while(binaryValue > 0)
             {
-                Decimal = Decimal + (int)Math.Pow(2,i);
+                temp = (binaryValue % 10);
+                Decimal = Decimal + (temp * (int)Math.Pow(2,i));
+                i++;
+                binaryValue = binaryValue / 10;
             }
-            Console.WriteLine("Binary: ${ Binary } is converted to Decimal: ${ Decimal }");
-            return Decimal;
+            string Result = $"Binary: {Binary} is converted to Decimal: {Decimal}";
+            return Result;
         }
+
+        public static string DecimalToBinary(int decimalValue)
+        {
+            string Result = $"Decimal: {decimalValue} is converted to Binary: ";
+            int value = decimalValue; int i;
+            int[] binaryString = new int[10];
+            for (i = 0; value > 0; i++)
+            {
+                binaryString[i] = (value % 2);
+                value = value / 2;
+            }
+            i--;
+            for (; i >= 0; i--)
+            {
+                Result += binaryString[i].ToString();
+            }
+            return Result;
+        }
+        public static string DecimalToOctal(int decimalValue)
+        {
+            string Result = $"Decimal: {decimalValue} is converted to Octal: ";
+            int value = decimalValue; int i;
+            int[] octalString = new int[10];
+            for (i = 0; value > 0; i++)
+            {
+                octalString[i] = (value % 8);
+                value = value / 8;
+            }
+            i--;
+            for (; i >= 0; i--)
+            {
+                Result += octalString[i].ToString();
+            }
+            return Result;
+        }
+        public static string DecimalToHexadecimal(int decimalValue)
+        {
+            string Result = $"Decimal: {decimalValue} is converted to Hexadecimal: ";
+            int value = decimalValue; int i;
+            int[] hexadecimalString = new int[10];
+            for (i = 0; value > 0; i++)
+            {
+                hexadecimalString[i] = (value % 16);
+                value /= 16;
+            }
+            i--;
+            for (; i >= 0; i--)
+            {
+                switch (hexadecimalString[i])
+                {
+                    case 10: Result += "A"; break;
+                    case 11: Result += "B"; break;
+                    case 12: Result += "C"; break;
+                    case 13: Result += "D"; break;
+                    case 14: Result += "E"; break;
+                    case 15: Result += "F"; break;
+                    default: Result += hexadecimalString[i].ToString(); break;
+                }
+            }
+            return Result;
+        }
+
         static void Main(string[] args)
         {
-            BinaryToDecimal("101");
+            Console.WriteLine(BinaryToDecimal("1000"));
+            Console.WriteLine(DecimalToBinary(8));
+            Console.WriteLine(DecimalToOctal(16));
+            Console.WriteLine(DecimalToHexadecimal(16));
         }
     }
 }
